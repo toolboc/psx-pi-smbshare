1c46f4f6e4c10582f6687e6e28be10bd1f5f492a
Paul DeCarlo <pdecarlo@microsoft.com>
2018-07-09 20:25:01 -0500

    Fix external network access on hostapd connections

--- setup-wifi-access-point.sh
+++ setup-wifi-access-point.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
 #
-# psx-pi-smbshare setup script
+# psx-pi-smbshare setup-wifi-access-point script
 #
 # *What it does*
 # This script will install and configure an external wifi dongle for access to XlinkKai & samba
 # This allows for configuring devices without ethernet to have access to these services
 # The default SSID name for the wifi network is XlinkKai
 #
-# Be sure to change the wpa_passphrase to a custom passworkd
-
-# Update packages
-sudo apt-get install -y hostapd bridge-utils
-sudo systemctl stop hostapd
+# Be sure to change the wpa_passphrase to a custom password
 
 # Configure hostapd
 sudo cat <<'EOF' | sudo tee /etc/default/hostapd
@@ -25,26 +21,22 @@ interface=wlan1
 ssid=XlinkKai
 hw_mode=g
 channel=7
+#ieee80211n=1
 wmm_enabled=0
 macaddr_acl=0
 auth_algs=1
 ignore_broadcast_ssid=0
 wpa=3
-wpa_passphrase=<YOURPASSWORD>
+wpa_passphrase=XlinkKai
 wpa_key_mgmt=WPA-PSK
 wpa_pairwise=TKIP
 rsn_pairwise=CCMP
 EOF
 
-sudo systemctl start hostapd
-
 # Configure dhcpcd
-
 sudo cat <<'EOF' | sudo tee /etc/dhcpcd.conf
 interface wlan1
-static ip_address=192.168.2.3/24
-static domain_name_servers=192.168.2.1 8.8.8.8
-static routers=192.168.2.1
+static ip_address=192.168.3.1/24
 nohook wpa_supplicant
 EOF
 
@@ -52,12 +44,17 @@ EOF
 sudo cat <<'EOF' | sudo tee -a /etc/dnsmasq.d/custom-dnsmasq.conf
 
 interface=wlan1
-dhcp-range=192.168.2.3,192.168.2.100,12h
+bind-dynamic
+dhcp-range=192.168.3.2,192.168.3.100,12h
 EOF
 
 # Forward additional ports
-sudo iptables -A FORWARD -i wlan1 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A FORWARD -i wlan0 -o wlan1 -j ACCEPT
+#sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
+sudo iptables -A FORWARD -i wlan0 -o wlan1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -p ALL -i wlan1 -o wlan0 -j ACCEPT
 
-# Restart dnsmasq
-sudo systemctl restart dnsmasq
+# Restart services
+sudo systemctl stop hostapd
+sudo systemctl stop dnsmasq
+sudo systemctl start hostapd
+sudo systemctl start dnsmasq
---
+++ wifi-to-eth-route.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Share Wifi with Eth device
+#
+#
+# This script is created to work with Raspbian Stretch
+# but it can be used with most of the distributions
+# by making few changes.
+#
+# Make sure you have already installed `dnsmasq`
+# Please modify the variables according to your need
+# Don't forget to change the name of network interface
+# Check them with `ifconfig`
+
+ip_address="192.168.2.1"
+netmask="255.255.255.0"
+dhcp_range_start="192.168.2.2"
+dhcp_range_end="192.168.2.100"
+dhcp_time="12h"
+eth="eth0"
+wlan="wlan0"
+
+sudo systemctl start network-online.target &> /dev/null
+
+sudo iptables -F
+sudo iptables -t nat -F
+sudo iptables -t nat -A POSTROUTING -o $wlan -j MASQUERADE
+sudo iptables -A FORWARD -i $wlan -o $eth -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i $eth -o $wlan -j ACCEPT
+
+sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+
+sudo ifconfig $eth $ip_address netmask $netmask
+
+# Remove default route created by dhcpcd
+sudo ip route del 0/0 dev $eth &> /dev/null
+
+sudo systemctl stop dnsmasq
+
+sudo rm -rf /etc/dnsmasq.d/*
+
+echo -e "interface=$eth\n\
+bind-dynamic\n\
+server=8.8.8.8\n\
+domain-needed\n\
+bogus-priv\n\
+dhcp-range=$dhcp_range_start,$dhcp_range_end,$dhcp_time" > /etc/dnsmasq.d/custom-dnsmasq.conf
+
+sudo systemctl start dnsmasq
