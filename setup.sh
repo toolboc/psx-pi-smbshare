6f925b5ec2356b88f7ce6e9589878dea4f9396d1
aapokaapo <aapok98@gmail.com>
2020-04-09 10:00:42 +0300

    Updated to latest XLink Kak

--- setup.sh
+++ setup.sh
@@ -45,14 +45,14 @@ wget https://raw.githubusercontent.com/toolboc/psx-pi-smbshare/master/setup-wifi
 chmod 755 /home/pi/setup-wifi-access-point.sh
 
 # Install Xlink Kai
-wget https://cdn.teamxlink.co.uk/binary/kaiEngine-7.4.33-rev767.headless.ARM.tar.gz
-tar -xzvf kaiEngine-7.4.33-rev767.headless.ARM.tar.gz
-sudo cp kaiEngine-7.4.33/kaiengine_arm /usr/local/bin
+wget https://cdn.teamxlink.co.uk/binary/kaiEngine-7.4.35-534304363.headless.armhf.tar.gz
+tar -xzvf kaiEngine-7.4.35-534304363.headless.armhf.tar.gz
+sudo cp kaiEngine-7.4.35/kaiengine /usr/local/bin
 sudo mkdir /root/.xlink
 
 cat <<'EOF' > /home/pi/launchkai.sh
 while true; do
-    /usr/local/bin/kaiengine_arm
+    /usr/local/bin/kaiengine
     sleep 1
 done
 EOF
